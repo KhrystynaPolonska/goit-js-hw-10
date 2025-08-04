c536fff1e9ddef9e238a395e36f638f8e77a8500
kristy <krisposol7@gmail.com>
2025-08-04 11:10:28 +0200

    add hw

---
+++ src/js/1-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const datetimePicker = document.getElementById("datetime-picker");
+const startBtn = document.getElementById("start-btn");
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+let userSelectedDate = null;
+let timerInterval = null;
+
+const picker = flatpickr(datetimePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate > new Date()) {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+    } else {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+      });
+      startBtn.disabled = true;
+    }
+  },
+});
+
+startBtn.addEventListener("click", () => {
+  if (!userSelectedDate) return;
+
+  startBtn.disabled = true;
+  datetimePicker.disabled = true;
+
+  timerInterval = setInterval(() => {
+    const now = new Date();
+    const diff = userSelectedDate - now;
+
+    if (diff <= 0) {
+      clearInterval(timerInterval);
+      updateTimerUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      datetimePicker.disabled = false;
+      return;
+    }
+
+    const timeData = convertMs(diff);
+    updateTimerUI(timeData);
+  }, 1000);
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateTimerUI({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
